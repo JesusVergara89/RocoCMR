672123a32f716e461c0cca118cccdafca82d79f8
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-10-14 14:09:58 -0600

    creating a signal to register when and what part of the product is modify create_product_history to save this record for future references

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, ProductHistory
 
 class ProductAdmin(admin.ModelAdmin):
     model = Product
@@ -7,5 +7,12 @@ class ProductAdmin(admin.ModelAdmin):
     search_fields = ("name", "description")
     list_display_links = ('id', 'name')
 
+class ProductHistoryAdmin(admin.ModelAdmin):
+    model = ProductHistory
+    list_display = ("id", "product", "created", "updated", "name", "price", "quantity", "available")
+    search_fields = ("product", "name")
+    list_display_links = ('id', 'product')  
+
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductHistory, ProductHistoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
@@ -13,3 +14,26 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+    
+class ProductHistory(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'History of {self.product.name} at {self.created}'
+
+
+@receiver(post_save, sender=Product)
+def create_product_history(sender, instance, created, **kwargs):
+    ProductHistory.objects.create(
+        product=instance,
+        name=instance.name,
+        price=instance.price,
+        quantity=instance.quantity,
+        available=instance.available
+    )
