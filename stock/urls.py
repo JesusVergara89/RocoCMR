a21ce8ab3760635a9485c0f26e285b4ab2d361a0
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-11-01 17:18:55 -0600

    new views in the stock app, CityStockView, CityStockView as a general view of the stock app, CityView as a view of all cities. Also inlcuding city_stock and cities respectively as a templates of the views. Adding the urls and all the thing for work correctly

--- stock/urls.py
+++ stock/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import StockFormView
+from .views import CityStockView, CityView, CityFormView
 
 urlpatterns = [
-    path("", StockFormView.as_view(), name="add_city"),
+    path("", CityStockView.as_view(), name="city_stock"),
+    path('cities/', CityView.as_view(), name="cities"),
+    path('add_city/', CityFormView.as_view(), name="add_city"),
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,17 +1,26 @@
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
 from .forms import CityForm, StockForm
 from django.urls import reverse_lazy
 from django.shortcuts import render, get_object_or_404
 from .models import City, Stock
 
-class StockFormView(LoginRequiredMixin, generic.FormView):
-    template_name = "stock/add_city.html"
 
-    form_class = StockForm
-    success_url = reverse_lazy("add")
+class CityStockView(LoginRequiredMixin,TemplateView):
+    template_name = "stock/city_stock.html"
+
+class CityView(LoginRequiredMixin, ListView):
+    model = City
+    template_name = "stock/cities.html"
+    context_object_name = "cities"    
+
+class CityFormView(LoginRequiredMixin, generic.FormView):
+    template_name = "stock/add_city.html"
+    form_class = CityForm
+    success_url = reverse_lazy("cities")
 
     def form_valid(self, form):
         form.save()
-        return super().form_valid(form)  
+        return super().form_valid(form)
 
