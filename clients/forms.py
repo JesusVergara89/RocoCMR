b92d4f7271b151e3de48ae8761aa0529d306f6c7
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-11-24 19:56:29 -0700

    addding new labels to the forms in clients, also adding new labels to the model in the clients for requirements to the client, those labels are city, state, tax identification number, tax regime, social name

--- clients/forms.py
+++ clients/forms.py
@@ -2,10 +2,22 @@ from django import forms
 from django.forms import ModelForm
 from .models import Client
 
+
 class ClientForm(ModelForm):
     class Meta:
         model = Client
-        fields = ('name', 'last_name','business_name' ,'email', 'phone', 'address', 'is_active')
-        widgets = {
-            'is_active': forms.CheckboxInput
-        }
+        fields = (
+            "name",
+            "last_name",
+            "business_name",
+            "email",
+            "phone",
+            "address",
+            "is_active",
+            "city",
+            "state",
+            "tax_identification_number",
+            "tax_regime",
+            "social_name",
+        )
+        widgets = {"is_active": forms.CheckboxInput}
--- clients/models.py
+++ clients/models.py
@@ -3,15 +3,31 @@ from django.contrib.auth.models import User
 
 
 class Client(models.Model):
+
+    STATE_CHOICES = [
+        ('autoretenedor', 'Autoretenedor'),
+        ('regimen_simplificado', 'Régimen Simplificado'),
+    ] 
+
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     business_name = models.CharField(max_length=100, null=True, blank=True)
+    social_name = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField()
     phone = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    city = models.CharField(max_length=150, null=True, blank=True)
+    state = models.CharField(max_length=150, null=True, blank=True)
+    tax_identification_number = models.CharField(max_length=150, null=True, blank=True)
+    tax_regime = models.CharField(
+        max_length=150, 
+        choices=STATE_CHOICES, 
+        null=True, 
+        blank=True
+    )
     sales_associate = models.ForeignKey(
         User, null=True, blank=True, on_delete=models.SET_NULL
     )
